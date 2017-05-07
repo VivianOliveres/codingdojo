e48725112d135f5d74c8608228ce375acfe8a392
Vivian Oliveres <v.oliveres@gmail.com>
2017-05-07 22:54:10 +0200

    BankOCR: Add use cases for user story 1

--- src/test/scala/com/kensai/codingdojo/algo/bank/AccountReaderSuite.scala
+++ src/test/scala/com/kensai/codingdojo/algo/bank/AccountReaderSuite.scala
@@ -25,4 +25,21 @@ class AccountReaderSuite extends FunSuite with Matchers{
     account2 shouldEqual new Account(0, 1, 8, 5, 6, 1, 3, 9, 7)
   }
 
+  test("user_story_1.txt") {
+    val accounts: List[Account] = reader.readAccount("src/test/resources/bank/user_story_1.txt")
+    accounts should have size 11
+
+    accounts should contain (new Account(0, 0, 0, 0, 0, 0, 0, 0, 0))
+    accounts should contain (new Account(1, 1, 1, 1, 1, 1, 1, 1, 1))
+    accounts should contain (new Account(2, 2, 2, 2, 2, 2, 2, 2, 2))
+    accounts should contain (new Account(3, 3, 3, 3, 3, 3, 3, 3, 3))
+    accounts should contain (new Account(4, 4, 4, 4, 4, 4, 4, 4, 4))
+    accounts should contain (new Account(5, 5, 5, 5, 5, 5, 5, 5, 5))
+    accounts should contain (new Account(6, 6, 6, 6, 6, 6, 6, 6, 6))
+    accounts should contain (new Account(7, 7, 7, 7, 7, 7, 7, 7, 7))
+    accounts should contain (new Account(8, 8, 8, 8, 8, 8, 8, 8, 8))
+    accounts should contain (new Account(9, 9, 9, 9, 9, 9, 9, 9, 9))
+    accounts should contain (new Account(1, 2, 3, 4, 5, 6, 7, 8, 9))
+  }
+
 }
